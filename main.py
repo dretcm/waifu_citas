032ee2dc702154f3109a0e42edaa05e5d3e705b8
Alexander Carpio Mamani <62957804+dretcm@users.noreply.github.com>
2021-06-21 13:26:23 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import pygame, sys
-from pygame.locals import *
 import random
 
 pygame.init()
 pygame.mixer.init()
 
-class Cita:
-        scena = 0
-        limite_scenas = 3
-        
-        font = pygame.font.Font(None,50)
+class Click:
+        def __init__(self, sound = "musica/click.mp3"):
+                self.click = pygame.mixer.Sound(sound)
+        def play_sound(self, sonido):
+                sound = pygame.mixer.Sound(sonido)
+                sound.play()
 
+class Cita(Click):
         def __init__(self, window_size, waifu):  # construye un nuevo objeto con nuevas variables.
+                super().__init__()
+                self.scena = 0
+                self.limite_scenas = 5
+                
+                self.font = pygame.font.Font(None,50)
+        
                 self.window_size = window_size
                 self.resultado = []
                 self.boton = pygame.transform.scale(pygame.image.load('imagenes/boton.jpg'), (window_size[0]-200, 40))
@@ -36,13 +43,13 @@ class Cita:
 
                 self.update()
 
-        def select_option(self, event, click):
+        def select_option(self, event):
                 if event.type == pygame.MOUSEBUTTONDOWN:
                         for i in range(len(self.colliders)):
                                 if event.button == 1 and self.colliders[i].collidepoint(event.pos):
                                         self.resultado.append(self.respuestas[self.scena][i])
                                         self.scena += 1
-                                        click.play()
+                                        self.click.play()
                                         if self.scena < self.limite_scenas:
                                                 self.update()
                 
@@ -77,23 +84,21 @@ class Cita:
         def __str__(self):
                 return str(int((sum(self.resultado)/len(self.resultado))*100)) + "%"
 
-class Game:
-        clock = pygame.time.Clock()
-        WINDOW_SIZE = (1200,600)
-        screen = pygame.display.set_mode(WINDOW_SIZE)
-        data = []
+class Game(Click):
+        def __init__(self, data):
+                super().__init__()
+                self.clock = pygame.time.Clock()
+                self.WINDOW_SIZE = (1200,600)
+                self.screen = pygame.display.set_mode(self.WINDOW_SIZE)
+                self.data = data
 
-        fondo = pygame.transform.scale(pygame.image.load('imagenes/fondo.jpg'), WINDOW_SIZE)
+                self.fondo = pygame.transform.scale(pygame.image.load('imagenes/fondo.jpg'), self.WINDOW_SIZE)
 
-        pygame.mixer.music.load('musica/fondo.mp3')
-        pygame.mixer.music.play(loops=-1)
-
-        click = pygame.mixer.Sound("musica/click.mp3")
+                pygame.mixer.music.load('musica/fondo.mp3')
+                pygame.mixer.music.play(loops=-1)
 
         def run_game(self):
-                waifu_1, waifu_2 = random.choices(self.data, k=2)
-                while id(waifu_1) == id(waifu_2):
-                        waifu_1, waifu_2 = random.choices(self.data, k=2)
+                waifu_1, waifu_2 = self.selecionar_waifus()
                         
                 cita_1 = Cita(self.WINDOW_SIZE, waifu_1)
                 cita_2 = Cita(self.WINDOW_SIZE, waifu_2)
@@ -102,9 +107,9 @@ class Game:
                 actual = 0
                 while True:
                         for event in pygame.event.get():
-                                if event.type == QUIT:
+                                if event.type == pygame.QUIT:
                                         self.exit_game()
-                                self.citas[actual].select_option(event, self.click)
+                                self.citas[actual].select_option(event)
 
 
                         self.screen.fill((0,0,0))
@@ -156,16 +161,16 @@ class Game:
 
 
                 acepta = pygame.transform.scale(pygame.image.load('imagenes/aceptar.png'), (80,50))
-                sound_acepta = pygame.mixer.Sound("musica/acepta.mp3")
+                sound_acepta = "musica/acepta.mp3"
                 
                 rechaza = pygame.transform.scale(pygame.image.load('imagenes/rechazo.png'), (80,50))
-                sound_rechazo = pygame.mixer.Sound("musica/rechazo.mp3")
+                sound_rechazo = "musica/rechazo.mp3"
 
                 opcion = None
 
                 while True:
                         for event in pygame.event.get():
-                                if event.type == QUIT:
+                                if event.type == pygame.QUIT:
                                         self.exit_game()
                                 if event.type == pygame.MOUSEBUTTONDOWN:
                                         if event.button == 1 and yes.collidepoint(event.pos):
@@ -177,19 +182,19 @@ class Game:
                                         if event.button == 1 and left_collider.collidepoint(event.pos) and right_on != True and left_on != True:
                                                 left_on = True
                                                 if random.choice(self.citas[0].resultado):
-                                                        sound_acepta.play()
+                                                        self.play_sound(sound_acepta)
                                                         opcion = acepta
                                                 else:
                                                         opcion = rechaza
-                                                        sound_rechazo.play()
+                                                        self.play_sound(sound_rechazo)
                                         if event.button == 1 and right_collider.collidepoint(event.pos) and right_on != True and left_on != True:
                                                 right_on = True
                                                 if random.choice(self.citas[1].resultado):
                                                         opcion = acepta
-                                                        sound_acepta.play()
+                                                        self.play_sound(sound_acepta)
                                                 else:
                                                         opcion = rechaza
-                                                        sound_rechazo.play()
+                                                        self.play_sound(sound_rechazo)
                                         
                         self.screen.blit(self.fondo, (0,0))
 
@@ -214,6 +219,13 @@ class Game:
                         
                         pygame.display.update()
                         self.clock.tick(30)
+
+        def selecionar_waifus(self):  # recursividad
+                waifu_1, waifu_2 = random.choices(self.data, k=2)
+                if id(waifu_1) != id(waifu_2):
+                        return waifu_1, waifu_2
+                else:
+                     return self.selecionar_waifus()
                         
         def exit_game(self):
                 pygame.quit()
@@ -222,31 +234,104 @@ class Game:
         def set_waifus(self, data):
                 self.data = data
 
-
 a = {
-        'dialogo':['hola ?', ' jajaja me gradas', 'casemos'],
-        'opciones':[['si eto','safa','ptmr'],
-                    ['vete ctmr', 'a noma', 'XD'],
-                    ['no!', 'me gustaria tener 17 hijos', 'carajo']],
-        'respuestas':[[ False,True, False], [True,  False, False], [False, True, False]],
-        'imagenes':['imagenes/w1.png', 'imagenes/w2.png', 'imagenes/w3.png'],
-        'personaje':'imagenes/w.png'
+        'dialogo':[' Que hace un extraño aqui? !!! ...',
+                   ' no eres un acosador verdad? !!! ...',
+                   ' mejor llamare a la policia ...',
+                   ' a...aaa!!!, que quieres decir?!',
+                   ' hubieras empezado con eso ...'],
+        'opciones':[['tú que haces aqui!','...','yo soy tu padre!'],
+                    ['solo si tú lo quisieras', 'solo fue una vez!', 'y si lo fuera, qué harias?'],
+                    ['por ser demasido guapo?', 'no tengo niños en el sotano!!!', 'y diles que anguien trata de robarme el corazon'],
+                    ['que soy traficador de organos', 'que fui tallado por los dioses', 'quiero salir contigo!'],
+                    ['me perdi en tu hermosura', 'queria ver como te ponias', 'queria invitarte un cevichito']],
+        'respuestas':[[True, False, False],
+                      [False,  False, True],
+                      [False, False, True],
+                      [False,  False, True],
+                      [False, False, True]],
+        'imagenes':['imagenes/itsuki_1.jpg',
+                    'imagenes/itsuki_2.jpg',
+                    'imagenes/itsuki_3.jpg',
+                    'imagenes/itsuki_4.jpg',
+                    'imagenes/itsuki_5.jpg'],
+        'personaje':'imagenes/itsuki.jpg'
         }
 
-b = {
-        'dialogo':['yaharoo!', ' entonces estas bien ?', ' mejor llamare a la policia'],
-        'opciones':[['yaharooo','oe chivola rctmr','klla kchera'],
-                    ['supongo', 'a', 'a mi pichula!'],
-                    ['por ser demasido guapo?', 'adios', 'naice']],
-        'respuestas':[[ False,False, True], [True,  False, False], [False, True, False]],
-        'imagenes':['imagenes/miku_1.png', 'imagenes/miku_2.png', 'imagenes/miku_3.png'],
-        'personaje':'imagenes/miku.png'
+b = {"dialogo":["¡Hola!",
+                "¿Enserio crees eso?",
+                "Gracias, sabes me gusta hacer deporte",
+                "Genial, me gustaria hacerlo juntos algunas vez",
+                "¡Maravilloso!, eres muy interesante y te pareces mucho a mi"],
+        "opciones":[["¿Hola como estas?","Te ves muy linda hoy","Te queda bien ese moño"],
+                    ["No, era broma","Si, combina con tu forma de vestir","Si jaja"],
+                    ["A mi no, son muy agotadores","A mi tambien, en especial el futbol","Prefiero ver peliculas"],
+                    ["igualmente, lo esperare con ansias","No creo que tenga tiempo","La verdad no tengo interes en eso"],
+                    ["Igualmente, siento que nos llevaremos bien","No lo creo pero gracias por eso, supongo","No vemos hasta la proxima"]],
+        "respuestas":[[False,False,True],
+                      [False,True,False],
+                      [False,True,False],
+                      [True,False,False],
+                      [True,False,False]],
+        "imagenes":["imagenes/yotsuba_5.jpg",
+                    "imagenes/yotsuba_2.jpg",
+                    "imagenes/yotsuba_1.jpg",
+                    "imagenes/yotsuba_4.jpg",
+                    "imagenes/yotsuba_3.jpg"], 
+        "personaje":"imagenes/yotsuba_ultima.jpg"
         }
 
-data = [a, b]
+c = {
+        'dialogo':[' Ohayoo conero-kun',
+                   ' ¿Listo para el trabajo de hoy?',
+                   ' ¿Te molesta algo conero-kun?',
+                   ' ¿Harás todo lo que te pida verdad?',
+                   ' Muy bien denji'],
+        'opciones':[['Hi?','...','Hola...'],
+                    ['Si, siempre listo', 'Tendré que ir de nuevo con Power', 'Aki siempre anda molesto'],
+                    ['No, todo bien', 'Esto es muy difícil', 'Trabajar con esos pendejos es un martirio'],
+                    ['Siempre', 'woaff', 'Como sea'],
+                    ['…', '…Gracias', 'adios makima']],
+        'respuestas':[[False, True, False],
+                      [True,  False, False],
+                      [True, False, False],
+                      [False,  True, False],
+                      [False, False, True]],
+        'imagenes':['imagenes/Makima1.png',
+                    'imagenes/Makima2.jpg',
+                    'imagenes/Makima3.png',
+                    'imagenes/Makima4.png',
+                    'imagenes/Makima5.png'],
+        'personaje':'imagenes/Makima.png'
+        }
+
+d = {
+        'dialogo':["Quien eres?",
+                   "Es la primera vez que te veo",
+                   "Solo te ponia a prueba",
+                   "Bueno, entonces crees que podriamos salir?",
+                   "Tengo cosas que hacer, luego seguimos hablando"],
+        'opciones':[["Tu admirador Nº1", "Era tu amigo", "Ni yo me acuerdo"],
+                    ["Tambien es mi primera vez", "Oh ya veo", "Que bueno"],
+                    ["Jaja que graciosa", "Mmm bueno","Te pasas ah"],
+                    ["Esta bien", "Bueno, nos vemos", "Todavia no te vayas, por favor"],
+                    ["Yo creo que sí", "Claro, tu solo dime cuando", "No estoy seguro, talvez"]],
+        'respuestas':[[True,False,False],
+                      [False,True,False],
+                      [True,False,False],
+                      [False,True,False],
+                      [False,True,False]],
+        'imagenes':['imagenes/mai_1.png',
+                    'imagenes/mai_2.png',
+                    'imagenes/mai_3.png',
+                    'imagenes/mai_4.png',
+                    'imagenes/mai_5.png'],
+        'personaje':'imagenes/mai.png'
+        }
+
+data = [a, b, c, d]
 
 
 if __name__ == '__main__':
-        game = Game()
-        game.set_waifus(data)
+        game = Game(data)
         game.run_game()
